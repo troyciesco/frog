472053d884f72174c4d47137c0d14ad46a192538
Troy Ciesco <tmciesco@gmail.com>
2025-05-06 13:42:37 -0400

    add family to redis

--- server/src/routes/admin.ts
+++ server/src/routes/admin.ts
@@ -9,7 +9,8 @@ import { Redis } from "ioredis"
 const connection = new Redis(
 	process.env.REDIS_URL ?? "redis://127.0.0.1:6379",
 	{
-		maxRetriesPerRequest: null
+		maxRetriesPerRequest: null,
+		family: 0
 	}
 )
 
--- worker/src/index.ts
+++ worker/src/index.ts
@@ -7,7 +7,8 @@ const sleep = (t: number) =>
 const connection = new Redis(
 	process.env.REDIS_URL ?? "redis://127.0.0.1:6379",
 	{
-		maxRetriesPerRequest: null
+		maxRetriesPerRequest: null,
+		family: 0
 	}
 )
 
